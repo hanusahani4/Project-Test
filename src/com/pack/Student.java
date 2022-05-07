bed6303bcab0f4b4cca817fb32804893be1b0621
hanu <hanusahani1@gmail.com>
2022-05-07 20:53:45 +0530

    first commit

---
+++ src/com/pack/Student.java
@@ -0,0 +1,21 @@
+package com.pack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Student {  
+	public static void main(String args[]){  
+		try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/school1","root","8182");  
+		//here sonoo is database name, root is username and password  
+		Statement stmt=con.createStatement();  
+		ResultSet rs=stmt.executeQuery("select * from register");  
+		while(rs.next())  
+		System.out.println("column value here");  
+		con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+		}  
+}
